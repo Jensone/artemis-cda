8bda14ff6a4390fea9c5045f0a685d54c3fbb6e8
Jensone <hello@jensone.com>
2023-12-11 12:25:20 +0100

    Refactor books.php layout

--- books.php
+++ books.php
@@ -1,21 +1,23 @@
-<?php 
+<?php
 
 namespace Artemis;
-
 require_once __DIR__ . '/src/entity/Book.php';
-
 use Artemis\Book;
-var_dump(Book::getAllBooks());
-die();
-
 
 include __DIR__ . '/templates/header.php';
 
 ?>
 
+<div class="mx-auto lg:ml-80">
+    <section class="py-8">
+        <div class="container px-4 mx-auto">
+            <div class="flex flex-wrap -m-4">
+                TEST
+            </div>
+        </div>
+    </section>
+</div>
 
-
-
-<?php 
+<?php
 
 include __DIR__ . '/templates/footer.php';
