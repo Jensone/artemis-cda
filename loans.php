ca9323d08d48946531621a2c13e89aa7ff56d3a7
Jensone <hello@jensone.com>
2023-12-11 19:56:24 +0100

    Refactor loan status handling in loans.php

--- loans.php
+++ loans.php
@@ -7,25 +7,32 @@ require_once __DIR__ . '/src/entity/Loan.php';
 use Artemis\Loan;
 
 $loans = Loan::getAllLoans();
-
 $today = date("Y-m-d");
-$inProgress = []; // Le livre est en cours d'emprunt
-$endSoon = []; // Le livre a dépassé la date de retour
-$isBack = []; // Le livre est rendu à la bibliothèque
 
-for ($i = 0; $i < count($loans); $i++) {
-    if ($loans[$i]['LoanStatus'] == 0) {
-        ($today <= $loans[$i]['LoanEndDate']) ? array_push($inProgress, $loans[$i]) : array_push($endSoon, $loans[$i]);
+$inProgress = [];
+$endSoon = [];
+$isBack = [];
+
+foreach ($loans as $loan) {
+    $loanStatus = $loan['LoanStatus'];
+    $loanEndDate = $loan['LoanEndDate'];
+
+    if ($loanStatus == 0) {
+        if ($today <= $loanEndDate) {
+            $inProgress[] = $loan;
+        } else {
+            $endSoon[] = $loan;
+        }
     } else {
-        array_push($isBack, $loans[$i]);
+        $isBack[] = $loan;
     }
 }
 
-$titles = ["En cours", "Terminé", "Rendu"];
-$colors = ["indigo", "orange", "green"];
+$loanStatusLabels = ['En cours', 'À rendre', 'Rendu'];
+$loanStatusColors = ['indigo', 'red', 'green'];
 
 include __DIR__ . '/templates/header.php';
-// include __DIR__ . '/templates/hero-books.php';
+include __DIR__ . '/templates/hero-loans.php';
 
 ?>
 
@@ -33,13 +40,12 @@ include __DIR__ . '/templates/header.php';
     <div class="container px-4 mx-auto">
         <div class="flex flex-wrap -m-4">
             <?php
-            for ($i = 0; $i < 3; $i++) {
-                $title = $titles[$i];
-                $color = $colors[$i];
-                //On boucle sur les tableau dans le template loans_card.php
+            for ($i = 0; $i < count($loanStatusLabels); $i++) {
+                $title = $loanStatusLabels[$i];
+                $color = $loanStatusColors[$i];
+                $array = ($i === 0) ? $inProgress : (($i === 1) ? $endSoon : $isBack);
                 include __DIR__ . '/templates/_partials/loans_column.php';
             }
-
             ?>
         </div>
     </div>
